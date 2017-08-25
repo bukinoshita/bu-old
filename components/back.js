ecfe3b9d5c42474cb950aed54ba9c1f30590a99f
Bu Kinoshita <bukinoshita@gmail.com>
2017-08-25 18:43:29 -0400

    fix `back` button clickable area (https://github.com/bukinoshita/bu/issues/3)

--- components/back.js
+++ components/back.js
@@ -1,24 +1,26 @@
-'use strict';
+'use strict'
 
-import Link from 'next/link';
+import Link from 'next/link'
 
 const Back = ({ inversed = false, href = '/' }) => {
   return (
     <div className={inversed ? 'inversed' : null}>
       <Link href={href}>
-        <svg
-          width="18"
-          height="18"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <line x1="20" y1="12" x2="4" y2="12" />
-          <polyline points="10 18 4 12 10 6" />
-        </svg>
+        <span>
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <line x1="20" y1="12" x2="4" y2="12" />
+            <polyline points="10 18 4 12 10 6" />
+          </svg>
+        </span>
       </Link>
 
       <style jsx>{`
@@ -41,6 +43,12 @@ const Back = ({ inversed = false, href = '/' }) => {
           transform: scale(1.1);
         }
 
+        span {
+          height: 30px;
+          width: 30px;
+          display: block;
+        }
+
         .inversed {
           background-color: #000;
         }
@@ -53,7 +61,7 @@ const Back = ({ inversed = false, href = '/' }) => {
         }
       `}</style>
     </div>
-  );
-};
+  )
+}
 
-export default Back;
+export default Back
