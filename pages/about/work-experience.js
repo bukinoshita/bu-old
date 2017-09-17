eddb880e4ea7f098f5a459811ef3af75998b4070
Bu Kinoshita <bukinoshita@gmail.com>
2017-09-17 10:45:47 -0400

    update range on `npmstat`

--- pages/about/work-experience.js
+++ pages/about/work-experience.js
@@ -15,7 +15,7 @@ import { colors, typography } from './../../theme'
 class WorkExperience extends Component {
   static async getInitialProps() {
     const { downloads } = await getDownloads('react-cookies', {
-      range: '2016-01-01:2017-09-10'
+      range: '2016-01-01:2017-09-17'
     })
 
     return { downloads }
