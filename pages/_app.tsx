23b8126a5260273b1f951670a27e405c2e810c0d
bukinoshita <bukinoshita@gmail.com>
2020-07-15 14:48:30 -0300

    feat: add fathom

---
+++ pages/_app.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
+
+const App = ({ Component, pageProps }: any) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_TRACKING_CODE || '', {
+      includedDomains: ['bukinoshita.com'],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
+  return <Component {...pageProps} />
+}
+
+export default App
